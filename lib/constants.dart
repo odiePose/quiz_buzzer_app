fdc9d34cd8c3ddc0d1e4bbe6d9e8594ec634db9d
Tobias <tobiasra@stud.ntnu.no>
2023-07-05 09:57:38 +0200

    Removing dotenv package from project

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,17 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 /// Environment variables and shared app constants.
-abstract class Constants {
+class Constants {
+  static const SUPABASE_API_KEY = String.fromEnvironment('SUPABASE_ANNON_KEY');
+  static const SUPABASE_URL = String.fromEnvironment('SUPABASE_URL');
+
   static List<String> getSupaBaseCredentials() {
-    final supabaseUrl = dotenv.env['SUPABASE_URL'];
-    final supabaseAnnonKey = dotenv.env['SUPABASE_ANNON_KEY'];
-    if (supabaseUrl == null || supabaseAnnonKey == null) {
-      throw Exception(
-          'Please define SUPABASE_URL and SUPABASE_ANNON_KEY in your .env file');
-    }
-    return [supabaseUrl, supabaseAnnonKey];
+    //final supabaseUrl = dotenv.env['SUPABASE_URL'];
+    //final supabaseAnnonKey = dotenv.env['SUPABASE_ANNON_KEY'];
+    //  if (supabaseUrl == null || supabaseAnnonKey == null) {
+    //    throw Exception(
+    //        'Please define SUPABASE_URL and SUPABASE_ANNON_KEY in your .env file');
+    //  }
+    print('SUPABASE_URL: $SUPABASE_URL');
+    print('SUPABASE_API_KEY: $SUPABASE_API_KEY');
+    return [SUPABASE_URL, SUPABASE_API_KEY];
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:beat_blitz/constants.dart';
 import 'package:beat_blitz/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  await dotenv.load();
+  //await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   final credentials = Constants.getSupaBaseCredentials();
   await Supabase.initialize(url: credentials[0], anonKey: credentials[1]);
